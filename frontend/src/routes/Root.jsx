239819691b03c6458deaac8838f2ad1d712c6658
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-08-29 14:09:43 -0600

    getting my template set up

---
+++ frontend/src/routes/Root.jsx
@@ -0,0 +1,43 @@
+import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import Messages from "../components/Messages";
+import { useEffect } from "react";
+
+function Root() {
+  const [user, setUser] = useState();
+  const [messages, setMessages] = useState({});
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then((res) => setUser(res.user));
+  }, []);
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        e.preventDefault();
+        setMessages({});
+      }
+    };
+    window.addEventListener("keydown", listener);
+    return () => window.removeEventListener("keydown", listener);
+  }, []);
+
+  return (
+    <>
+      <header className="container">
+        <div className="text-center">
+          <h1 className="">
+            <Link to={user ? "/profile" : "/"}>Budget Breeze</Link>
+          </h1>
+          <span>Breathe Easy. Budget Smart.</span>
+        </div>
+      </header>
+      <Messages messages={messages} />
+      <Outlet context={{ user, setUser, setMessages }} />
+    </>
+  );
+}
+
+export default Root;
